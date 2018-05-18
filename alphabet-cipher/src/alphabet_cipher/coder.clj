9576b863a30b713025a4ffa9375204697f52b82f
Pavel Lengarov <pavel.lengarov@circles.bz>
2018-05-18 17:27:04 +0300

    Regular check in.

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,5 +1,18 @@
 (ns alphabet-cipher.coder)
 
+(defn gen-alphabet []
+  (vec (map char (range 97 123))))
+
+(defn rotate-seq [n coll]
+  (take (count coll)
+        (drop (mod n (count coll))
+              (cycle coll))))
+
+(defn div-idxs [idx1 idx2]
+  (Math/abs (- (int idx1) (int idx2))))
+
+
+
 (defn encode [keyword message]
   "encodeme")
 
@@ -8,4 +21,3 @@
 
 (defn decipher [cipher message]
   "decypherme")
-
