c6adc195bd80fdeb985db25198fbb49e97313494
Pavel Lengarov <pavel.lengarov@circles.bz>
2018-06-07 17:45:03 +0300

    War game solution.

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,44 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn positions
+  [pred coll]
+  (keep-indexed (fn [idx x]
+                  (when (pred x)
+                    idx))
+                coll))
 
-(defn play-game [player1-cards player2-cards])
+(defn compare-ranks
+  [card1 card2]
+  (let [[rank1] (positions #{(card1 1)} ranks)
+        [rank2] (positions #{(card2 1)} ranks)]
+    (- rank1 rank2)))
+
+(defn compare-suits
+  [card1 card2]
+  (let [[suit1] (positions #{(card1 0)} suits)
+        [suit2] (positions #{(card2 0)} suits)]
+    (- suit1 suit2)))
+
+(defn compare-cards
+  [card1 card2]
+  (let [rank-res (compare-ranks card1 card2)
+        suit-res (compare-suits card1 card2)]
+    (or (> rank-res 0)
+        (and (= rank-res 0)
+             (< suit-res 0)))))
+
+(defn play-round
+  [card1 card2]
+  (compare-cards card1 card2))
+
+(defn play-game
+  [deck1 deck2]
+  (cond
+    (empty? deck1) "Player2 wins"
+    (empty? deck2) "Player1 wins"
+    :else (let [[card1 & rest-deck1] deck1
+                [card2 & rest-deck2] deck2]
+            (if (play-round card1 card2)
+              (recur (into rest-deck1 [card1 card2]) rest-deck2)
+              (recur rest-deck1 (into rest-deck2 [card1 card2]))))))
